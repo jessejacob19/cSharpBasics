8cb8c682357fe1ca3a1dfcdba07adebf5178a92d
jesse collier <jesse.collier97@gmail.com>
2019-06-09 16:35:49 +1200

    did string methods

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -6,6 +6,7 @@ namespace cSharpBasics
     {
         static void Main(string[] args)
         {
+            //tutoral by https://www.youtube.com/watch?v=GhQdlIFylQ8&t=264s
             // core data types
             string str = "this is a string";
             char c = 'c'; //only use single quotes for char double quotes for string
@@ -14,8 +15,27 @@ namespace cSharpBasics
             double d = 1.3345; // has to have the point
             bool isCorrect = true; // basic boolean
 
+            //strings in c#
+            Console.WriteLine("hello\nworld");//\n is the newline character
+            string concat = "hi" + " sup";
+            Console.WriteLine(concat.Length);//prints the length
+            Console.WriteLine(concat.ToUpper());//makes all the letters uppercase
+            Console.WriteLine(concat.Contains("hi"));//returns boolean
+            Console.WriteLine(concat[0]);//indexing the character
+            Console.WriteLine(concat.IndexOf("hi"));//pos where it first starts
+            Console.WriteLine(concat.IndexOf("z");// if its not in there
+            Console.WriteLine(concat.Substring(4, 2));// pos 4 onwards with a length of two
+                                                      //c# string methods
 
+            //numbers
 
+            Console.WriteLine(5 % 2);//how to use modulus
+
+            //when int / int you only get an int
+            Console.WriteLine(5/2);//
+
+            //when int / float you get a float
+            Console.WriteLine(5/2.0);//
 
 
             //pointers
