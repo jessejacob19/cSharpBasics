e42439dad17616c954e956622ee01fd3ef501815
jesse collier <jesse.collier97@gmail.com>
2019-06-09 21:55:46 +1200

    added exceptions

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -159,10 +159,37 @@ namespace cSharpBasics
              multi line
              
              */
+
+            //exception handling
+
+            exceptions();
             Console.ReadLine();
         }
 
+        static void exceptions()
+        {
+            try
+            {
+                Console.Write("Enter a Number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another Number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine(num1 / num2);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                //does code no matter what
+            }
+        }
         static void twoDArray ()
         {
             //everything that you would do in a normal array
