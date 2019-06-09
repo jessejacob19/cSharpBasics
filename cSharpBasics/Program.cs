0920ff7a1a089b71e9f4b1bcacd67c3f6c1b8711
jesse collier <jesse.collier97@gmail.com>
2019-06-09 15:58:31 +1200

    added basic datatypes

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -6,7 +6,20 @@ namespace cSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // core data types
+            string str = "this is a string";
+            char c = 'c'; //only use single quotes for char double quotes for string
+            int num = 4;//integer is whole number
+            // float double decimal other examples of numbers least specific to most specific
+            double d = 1.3345; // has to have the point
+            bool isCorrect = true; // basic boolean
+
+
+
+
+
+            //pointers
+            Console.WriteLine("Hello World!" + "this is how you add strings together");// how to print on the console
         }
     }
 }
