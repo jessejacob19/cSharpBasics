a5c5d4489eb13114ed79f900c38b46f62def65ad
jesse collier <jesse.collier97@gmail.com>
2019-06-09 22:51:04 +1200

    constructor stuff done

--- cSharpBasics/Book.cs
+++ cSharpBasics/Book.cs
@@ -6,6 +6,13 @@ namespace cSharpBasics
         public string title;
         public string author;
         public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages )
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
     }
 }
 
--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -166,19 +166,15 @@ namespace cSharpBasics
 
             //classes
 
-            Book book1 = new Book();
+            Book book1 = new Book("w", "w", 2);
             book1.title = "Harry Potter";
             book1.author = "JK Rowling";
             book1.pages = 400;
 
             Console.WriteLine(book1.pages);
 
-            Book book2 = new Book();
-            book2.title = "Lord Of the Rings";
-            book2.author = "Tolkein";
-            book2.pages = 700;
+            Book book2 = new Book("Lord Of the Rings", "Tolkein", 700);
 
-            Console.WriteLine(book1.pages);
 
 
             Console.ReadLine();
